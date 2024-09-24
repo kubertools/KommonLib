5bf56ae8399e38a1cc08660deac4b7027bffec4b
kubermate <168834048+kubermate@users.noreply.github.com>
2024-09-24 22:45:19 +0700

    Add namespace registry

--- kommonlib/registry/__init__.py
+++ kommonlib/registry/__init__.py
@@ -1,1 +1,4 @@
-from .common import AbstractRegistry, REGISTRY_KEY, REGISTRY_VALUE
+from .common import REGISTRY_KEY, REGISTRY_VALUE
+
+from .common import AbstractRegistry
+from .namespace import AbstractNamespaceRegistry, NamespaceKey
